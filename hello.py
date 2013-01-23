149fa0645cbec8a63b1ccb9c063bee4fb10024f2
Jason Bandlow <jbandlow@gmail.com>
2013-01-23 15:41:56 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from sage.all import *
+
+app = Flask(__name__)
+
+@app.route("/<command>")
+def hello(command):
+  return """
+  <head>
+  <script type="text/x-mathjax-config">
+    MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+  </script>
+  <script type="text/javascript"
+    src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+  </script>
+  </head>
+  <body>
+  """ + "$$" + str(eval(command)) + "$$" + "</body>"
+
+if __name__ == "__main__":
+  app.run()
