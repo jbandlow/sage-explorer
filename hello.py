348aa50a00b785664cbe9800c7afee05ee2fe41a
Nicolas M. Thiéry <Nicolas.Thiery@u-psud.fr>
2013-01-23 16:25:52 +0000

    Added invariants_view

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
+import inspect
 from flask import Flask
-from sage.all import *
+import sage.all
 
 app = Flask(__name__)
 
@@ -15,14 +16,33 @@ def hello(command):
   </script>
   </head>
   <body>
-  """ + "$$" + str(eval(command)) + "$$" + "</body>"
+  """ + "$$" + latex(eval(command)) + "$$" + "</body>"
 
 def argument_less_methods_of_object(x):
   """
   Returns the list of the names of the methods of ``x`` that take no argument, excluding _methods
+
+  EXAMPLES::
+
+      sage: argument_less_methods_of_object(1)
+      ['is_idempotent']
   """
-  [ key for (key, f) in inspect.getmembers(x, inspect.ismethod) if len(inspect.getargspec(f).args)==1 and not key[0] == "_"]
+  return [ key for (key, f) in inspect.getmembers(x, inspect.ismethod) if len(inspect.getargspec(f).args)==1 and not key[0] == "_"]
 
+def invariants_view(self, command):
+    """
+    EXAMPLES::
 
-if __name__ == "__main__":
+        sage: print invariants_view(Permutation([2,1]), "foo")
+        <ul>
+        <a href='/foo.bruhat_greater()'>bruhat_greater</a>
+        <a href='/foo.bruhat_inversions()'>bruhat_inversions</a>
+        ...
+        </ul>
+    """
+    invariants = argument_less_methods_of_object(self)
+    return "<ul>\n"+"\n".join("<a href='/%s.%s()'>%s</a>"%(command, invariant, invariant) for invariant in invariants)+"\n</ul>\n"
+
+# Stupid test that we are not running within Sage
+if __name__ == "__main__" and not "Permutations" in globals():
   app.run()
